a2a98de30756d7a0876b4ef9a655d3099fd04ea2
karthiksatish159 <karthiksatish159@gmail.com>
2023-05-21 21:28:39 +0530

    swagger docs are added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 require('dotenv').config();
 import express,{Express,NextFunction,Request,Response} from "express";
 import { MongooseConnection } from "./config/db.setup";
+import basicAuth from 'express-basic-auth';
+import swaggerui from 'swagger-ui-express';
+import YAML from 'yamljs';
+import fs from 'fs';
 const app:Express=express();
 const port:number=3500||Number(process.env.PORT);
 app.use(express.json());
@@ -8,6 +12,21 @@ app.use(express.urlencoded({extended:false}));
 /** MongoDB connection setup*/
 const dbConnect=new MongooseConnection("db");
 dbConnect.connect().then(()=>{console.log('database connected')}).catch((err)=>{console.log(err)});
+//* @swagger setup
+const file=fs.readFileSync('src/api.yaml','utf-8');
+const swaggerDocument=YAML.parse(file);
+app.use('/api-docs',basicAuth(
+    {
+        challenge: true, // Sends a 401 Unauthorized response if authentication fails
+        realm:"events app",
+        authorizer: myAuthorizer
+    }),swaggerui.serve,swaggerui.setup(swaggerDocument))
+    function myAuthorizer(username:string,password:string):boolean
+    {
+      const userMatches=basicAuth.safeCompare(username,(process.env.SWAGGER_USER as string));
+      const passwordMatches =basicAuth.safeCompare(password,(process.env.SWAGGER_PASSWORD as string));
+      return userMatches && passwordMatches;
+    }
 app.get('/',(req:Request,res:Response)=>
 {
     res.send("<h1>Hello</h1>");
