1e685c043bcbc6203e58fb706e83fe441fa3fcfa
Karthiksatish <74001872+karthiksatish159@users.noreply.github.com>
2024-07-22 19:23:51 +0530

    refactored

--- src/index.ts
+++ src/index.ts
@@ -15,25 +15,25 @@ dbConnect.connect().then(()=>{console.log('database connected')}).catch((err)=>{
 //* @swagger setup
 const file=fs.readFileSync('src/api.yaml','utf-8');
 const swaggerDocument=YAML.parse(file);
-app.use('/api-docs',basicAuth(
-    {
+
+app.use('/api-docs',basicAuth({
         challenge: true, // Sends a 401 Unauthorized response if authentication fails
         realm:"events app",
         authorizer: myAuthorizer
-    }),swaggerui.serve,swaggerui.setup(swaggerDocument))
-    function myAuthorizer(username:string,password:string):boolean
-    {
+    }),swaggerui.serve,swaggerui.setup(swaggerDocument)
+)
+
+function myAuthorizer(username:string,password:string):boolean {
       const userMatches=basicAuth.safeCompare(username,(process.env.SWAGGER_USER as string));
       const passwordMatches =basicAuth.safeCompare(password,(process.env.SWAGGER_PASSWORD as string));
       return userMatches && passwordMatches;
-    }
+}
 app.get('/',(req:Request,res:Response)=>
 {
     res.send("<h1>Hello</h1>");
 });
 //* @routes
 app.use('/api/v3/app/',require('./routes/event.routes'));
-app.listen(port,():void=>
-{
+app.listen(port,():void=> {
     console.log(`The server is running on ${port}`);
 })
